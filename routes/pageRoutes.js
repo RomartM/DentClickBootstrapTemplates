42cbeffda1f17bfae3f76a5089ec2605b263df40
Romart Mediante <mediantekid@gmail.com>
2020-06-30 22:07:09 +0800

    Updated template routes for non-authenticated paths

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -8,6 +8,46 @@ module.exports = [
             return reply.view('layout/default')
         }
     },
+    {   // Clinic Main
+        method: 'GET',
+        path: '/clinic',
+        handler: (request, reply) => {
+            return reply.view('layout/clinic/main/main', { custom: {
+              style: true,
+              styleName: 'signin'
+            } })
+        }
+    },
+    {   // Clinic Login Email
+        method: 'GET',
+        path: '/clinic/login/email',
+        handler: (request, reply) => {
+            return reply.view('layout/clinic/main/login-email', { custom: {
+              style: true,
+              styleName: 'signin'
+            } })
+        }
+    },
+    {   // Clinic Register
+        method: 'GET',
+        path: '/clinic/register',
+        handler: (request, reply) => {
+            return reply.view('layout/clinic/register', { custom: {
+              style: true,
+              styleName: 'signin'
+            } })
+        }
+    },
+    {   // Clinic Setup
+        method: 'GET',
+        path: '/clinic/setup',
+        handler: (request, reply) => {
+            return reply.view('layout/clinic/setup', { custom: {
+              style: true,
+              styleName: 'signin'
+            } })
+        }
+    },
     {
         method: 'GET',
         path: '/{path*}',
